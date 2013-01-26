863f3923f4dacd6f59375912448c9bc8c11b9b57
Giorgio Borelli <casaborelli@gmail.com>
2013-01-26 17:43:57 +0100

    preparing release

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.5.dev0'
+version = '1.5dev'
 install_requires = [
     'setuptools',
     'Plone',
     'geopy',
 ]
+
 # Test if we have built-in JSON - Python 2.6+, 3.0+.
 # Older Python versions require simplejson.
 try:
-    import json
+    import json  # pylint: disable=W0611
 except ImportError:
     install_requires.append('simplejson')
 
@@ -18,14 +19,14 @@ setup(name='collective.geo.geographer',
       version=version,
       description="Geographic annotation for Plone",
       long_description=open(
-              "collective/geo/geographer/README.rst").read() + "\n" +
-                      open(os.path.join("docs", "HISTORY.txt")).read(),
+          "collective/geo/geographer/README.rst").read() + "\n" + open(
+              os.path.join("docs", "HISTORY.txt")).read(),
       classifiers=[
-        "Framework :: Plone",
-        "Topic :: Internet",
-        "Topic :: Scientific/Engineering :: GIS",
-        "Programming Language :: Python",
-        ],
+          "Framework :: Plone",
+          "Topic :: Internet",
+          "Topic :: Scientific/Engineering :: GIS",
+          "Programming Language :: Python",
+      ],
       keywords='Zope Plone GIS KML Google Maps Bing Yahoo OpenLayers',
       author='Giorgio Borelli',
       author_email='giorgio@giorgioborelli.it',
@@ -37,9 +38,9 @@ setup(name='collective.geo.geographer',
       zip_safe=False,
       install_requires=install_requires,
       extras_require={
-        'test': [
-            'plone.app.testing',
-        ],
+          'test': [
+              'plone.app.testing',
+          ],
       },
       entry_points="""
       # -*- Entry points: -*-
