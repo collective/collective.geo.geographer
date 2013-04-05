23c242233e03442229efb1ec493116ede6395afc
Valentina Bolognini <vale.asia@gmail.com>
2013-04-05 15:10:53 -0700

    Added a method in order to hidden Coordinates tab for dexterity content types

--- collective/geo/geographer/geoview.py
+++ collective/geo/geographer/geoview.py
@@ -1,11 +1,19 @@
+import pkg_resources
 from zope.interface import implements
 
 from Products.Five.browser import BrowserView
 
+try:
+    pkg_resources.get_distribution('plone.dexterity')
+except pkg_resources.DistributionNotFound:
+    HAS_DEXTERITY = False
+else:
+    HAS_DEXTERITY = True
+    from plone.dexterity.interfaces import IDexterityContent
+
 from .interfaces import IGeoreferenceable
 from .interfaces import IGeoreferenced
 from .interfaces import IGeoView
-
 
 class GeoView(BrowserView):
     """A simple view to know if an object is geo referenceable.
@@ -19,6 +27,15 @@ class GeoView(BrowserView):
 
     def isGeoreferenceable(self):
         return IGeoreferenceable.providedBy(self.context)
+
+    def isDexterityContentType(self):
+        if HAS_DEXTERITY:
+            return IDexterityContent.providedBy(self.context)
+        return False
+
+    def showCoordinatesTab(self):
+        # Dexterity content types don't need Coordinates Tab
+        return self.isGeoreferenceable() and not self.isDexterityContentType()
 
     def getCoordinates(self):
         if self.isGeoreferenceable():
