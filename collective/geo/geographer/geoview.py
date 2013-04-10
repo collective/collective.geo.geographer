14f36e58be5baebb1b71cbc8c2dcc3b423b81389
Giorgio Borelli <casaborelli@gmail.com>
2013-04-10 19:40:05 +0200

    Moved showCoordinatesTab to c.geo.contentlocations

--- collective/geo/geographer/geoview.py
+++ collective/geo/geographer/geoview.py
@@ -3,14 +3,6 @@ from zope.interface import implements
 
 from Products.Five.browser import BrowserView
 
-try:
-    pkg_resources.get_distribution('plone.dexterity')
-except pkg_resources.DistributionNotFound:
-    HAS_DEXTERITY = False
-else:
-    HAS_DEXTERITY = True
-    from plone.dexterity.interfaces import IDexterityContent
-
 from .interfaces import IGeoreferenceable
 from .interfaces import IGeoreferenced
 from .interfaces import IGeoView
@@ -28,15 +20,6 @@ class GeoView(BrowserView):
 
     def isGeoreferenceable(self):
         return IGeoreferenceable.providedBy(self.context)
-
-    def isDexterityContentType(self):
-        if HAS_DEXTERITY:
-            return IDexterityContent.providedBy(self.context)
-        return False
-
-    def showCoordinatesTab(self):
-        # Dexterity content types don't need Coordinates Tab
-        return self.isGeoreferenceable() and not self.isDexterityContentType()
 
     def getCoordinates(self):
         if self.isGeoreferenceable():
