e6783123b0a40489ce54279b715df60ccac22058
Matteo Parrucci <parruc@gmail.com>
2015-10-18 01:53:45 +0300

    starting to fix tests

--- src/collective/geo/geographer/testing.py
+++ src/collective/geo/geographer/testing.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from plone.app.contenttypes.testing import PLONE_APP_CONTENTTYPES_FIXTURE
 from plone.app.testing import PloneWithPackageLayer
 from plone.app.testing import IntegrationTesting
 from plone.app.testing import FunctionalTesting
@@ -7,15 +8,16 @@ import collective.geo.geographer
 
 
 CGEO_GEOGRAPHER = PloneWithPackageLayer(
+    defaultBases=(PLONE_APP_CONTENTTYPES_FIXTURE, ),
     zcml_package=collective.geo.geographer,
     zcml_filename='testing.zcml',
     gs_profile_id='collective.geo.geographer:default',
     name="CGEO_GEOGRAPHER")
 
 CGEO_GEOGRAPHER_INTEGRATION = IntegrationTesting(
-    bases=(CGEO_GEOGRAPHER, ),
+    defaultBases=(CGEO_GEOGRAPHER, ),
     name="CGEO_GEOGRAPHER_INTEGRATION")
 
 CGEO_GEOGRAPHER_FUNCTIONAL = FunctionalTesting(
-    bases=(CGEO_GEOGRAPHER, ),
+    defaultBases=(CGEO_GEOGRAPHER, ),
     name="CGEO_GEOGRAPHER_FUNCTIONAL")
